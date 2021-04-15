b0a3100e89f404676df5dbb6ce17010801d86c44
Changjian Gao <gaochangjian@gmail.com>
2021-04-15 16:09:04 +0800

    First commit

---
+++ docusaurus.config.js
@@ -0,0 +1,106 @@
+/** @type {import('@docusaurus/types').DocusaurusConfig} */
+module.exports = {
+  title: 'Maybe News',
+  tagline: '可能是新闻的知识',
+  url: 'https://maybe.news',
+  baseUrl: '/',
+  onBrokenLinks: 'throw',
+  onBrokenMarkdownLinks: 'warn',
+  favicon: 'img/favicon.ico',
+  organizationName: 'maybe-news', // Usually your GitHub org/user name.
+  projectName: 'maybe-news.github.io', // Usually your repo name.
+  stylesheets: [
+    'https://fonts.googleapis.com/css?family=Lora:400,400i,700,700i,900,900i|Nanum+Gothic+Coding:400,400i,700,700i,900,900i|Neuton:400,400i,700,700i,900,900i',
+  ],
+  themeConfig: {
+    colorMode: {
+      defaultMode: 'light',
+    },
+    navbar: {
+      title: 'Maybe News',
+      // logo: {
+      //   alt: 'Logo',
+      //   src: 'img/logo.svg',
+      // },
+      items: [
+        {to: 'issues', label: 'Issues', position: 'left', activeBasePath: 'issues'},
+        {to: 'about', label: 'About', position: 'left'},
+        // {
+        //   href: 'https://github.com/facebook/docusaurus',
+        //   label: 'GitHub',
+        //   position: 'right',
+        // },
+      ],
+    },
+    footer: {
+      style: 'dark',
+      links: [
+        // {
+        //   title: 'Docs',
+        //   items: [
+        //     {
+        //       label: 'Getting Started',
+        //       to: 'docs/',
+        //     },
+        //   ],
+        // },
+        // {
+        //   title: 'Community',
+        //   items: [
+        //     {
+        //       label: 'Stack Overflow',
+        //       href: 'https://stackoverflow.com/questions/tagged/docusaurus',
+        //     },
+        //     {
+        //       label: 'Discord',
+        //       href: 'https://discordapp.com/invite/docusaurus',
+        //     },
+        //     {
+        //       label: 'Twitter',
+        //       href: 'https://twitter.com/docusaurus',
+        //     },
+        //   ],
+        // },
+        // {
+        //   title: 'More',
+        //   items: [
+        //     {
+        //       label: 'Blog',
+        //       to: 'blog',
+        //     },
+        //     {
+        //       label: 'GitHub',
+        //       href: 'https://github.com/facebook/docusaurus',
+        //     },
+        //   ],
+        // },
+      ],
+      copyright: `Copyright © ${new Date().getFullYear()} Maybe News. Built with Docusaurus.`,
+    },
+  },
+  presets: [
+    [
+      '@docusaurus/preset-classic',
+      {
+        docs: {
+          sidebarPath: require.resolve('./sidebars.js'),
+        },
+        blog: {
+          blogTitle: 'Issues',
+          blogSidebarCount: 'ALL',
+          blogSidebarTitle: 'Archives',
+          routeBasePath: 'issues',
+          showReadingTime: false,
+          feedOptions: {
+            type: 'all',
+            title: 'Maybe News',
+            description: 'Maybe News',
+          },
+        },
+        theme: {
+          customCss: require.resolve('./src/css/custom.css'),
+        },
+      },
+    ],
+  ],
+};
---
+++ src/pages/index.js
@@ -0,0 +1,144 @@
+import React from 'react';
+import clsx from 'clsx';
+import Layout from '@theme/Layout';
+// import Link from '@docusaurus/Link';
+import useDocusaurusContext from '@docusaurus/useDocusaurusContext';
+// import useBaseUrl from '@docusaurus/useBaseUrl';
+import MailchimpSubscribe from 'react-mailchimp-subscribe';
+import styles from './styles.module.css';
+
+// const features = [
+//   {
+//     title: 'Easy to Use',
+//     imageUrl: 'img/undraw_docusaurus_mountain.svg',
+//     description: (
+//       <>
+//         Docusaurus was designed from the ground up to be easily installed and
+//         used to get your website up and running quickly.
+//       </>
+//     ),
+//   },
+//   {
+//     title: 'Focus on What Matters',
+//     imageUrl: 'img/undraw_docusaurus_tree.svg',
+//     description: (
+//       <>
+//         Docusaurus lets you focus on your docs, and we&apos;ll do the chores. Go
+//         ahead and move your docs into the <code>docs</code> directory.
+//       </>
+//     ),
+//   },
+//   {
+//     title: 'Powered by React',
+//     imageUrl: 'img/undraw_docusaurus_react.svg',
+//     description: (
+//       <>
+//         Extend or customize your website layout by reusing React. Docusaurus can
+//         be extended while reusing the same header and footer.
+//       </>
+//     ),
+//   },
+// ];
+
+// function Feature({imageUrl, title, description}) {
+//   const imgUrl = useBaseUrl(imageUrl);
+//   return (
+//     <div className={clsx('col col--4', styles.feature)}>
+//       {imgUrl && (
+//         <div className="text--center">
+//           <img className={styles.featureImage} src={imgUrl} alt={title} />
+//         </div>
+//       )}
+//       <h3>{title}</h3>
+//       <p>{description}</p>
+//     </div>
+//   );
+// }
+
+const CustomForm = ({ status, message, onValidated }) => {
+  let email;
+  const submit = () =>
+    email &&
+    email.value.indexOf("@") > -1 &&
+    onValidated({
+      EMAIL: email.value,
+    });
+
+  return (
+    <div className="container">
+      <div className="row">
+        <div className="col col--6 col--offset-3">
+          <div className={styles.subscribeForm}>
+            {status === "sending" && <div className="alert alert--info" role="alert">Sending...</div>}
+            {status === "error" && (
+              <div
+                role="alert"
+                className="alert alert--danger"
+                dangerouslySetInnerHTML={{ __html: message }}
+              />
+            )}
+            {status === "success" && (
+              <div
+                role="alert"
+                className="alert alert--success"
+                dangerouslySetInnerHTML={{ __html: message }}
+              />
+            )}
+            <div className={styles.formInputWrapper}>
+              <label className={styles.formLabel}>
+                Email Address *
+                <input
+                  className={styles.formInput}
+                  ref={node => (email = node)}
+                  type="email"
+                />
+              </label>
+            </div>
+            <div className={styles.subscribeBtnWrapper}>
+              <button className={styles.subscribeBtn} onClick={submit}>Subscribe</button>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default function Home() {
+  const context = useDocusaurusContext();
+  const {siteConfig = {}} = context;
+  return (
+    <Layout
+      title="Home"
+      description="可能是新闻的知识">
+      <header className={clsx('hero hero--primary', styles.heroBanner)}>
+        <div className="container">
+          <h1 className={clsx('hero__title', styles.heroTitle)}>{siteConfig.title}</h1>
+          <p className={clsx('hero__subtitle', styles.heroSubtitle)}>{siteConfig.tagline}</p>
+          {/* <div className={styles.buttons}> */}
+          {/*   <Link */}
+          {/*     className={clsx( */}
+          {/*       'button button--outline button--secondary button--lg', */}
+          {/*       styles.getStarted, */}
+          {/*     )} */}
+          {/*     to={useBaseUrl('docs/')}> */}
+          {/*     Get Started */}
+          {/*   </Link> */}
+          {/* </div> */}
+        </div>
+      </header>
+      <main>
+        <MailchimpSubscribe
+          url="https:news.us18.list-manage.com/subscribe/post?u=013b62af8e756cabe51db0038&id=fc8cfd980b"
+          render={({ subscribe, status, message }) => (
+            <CustomForm
+              status={status}
+              message={message}
+              onValidated={formData => subscribe(formData)}
+            />
+          )}
+        />
+      </main>
+    </Layout>
+  );
+}
