8331b167ba654239a133d9c4443fa33a6b2ad5d9
gh-actions <actions@github.com>
2022-05-26 02:53:03 +0000

    Deploy website - based on ba1170fd7d8b4d9526bac9f8b295c22157ae001d

---
+++ assets/js/8a4b4e1a.9678660e.js
@@ -0,0 +1,1 @@
+(window.webpackJsonp=window.webpackJsonp||[]).push([[28],{99:function(e){e.exports=JSON.parse('{"permalink":"/issues","page":1,"postsPerPage":10,"totalPages":2,"totalCount":14,"previousPage":null,"nextPage":"/issues/page/2","blogDescription":"Blog","blogTitle":"Issues"}')}}]);
